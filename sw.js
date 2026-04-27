350b98df13b03b670f595846e4178665b6e3c9f8
antigovernment <bean12810@gmail.com>
2026-04-26 20:13:51 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'calendar-v1';
+const urlsToCache = [
+  './',
+  './calendar.html',
+  './calendar.css',
+  './calendar.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
